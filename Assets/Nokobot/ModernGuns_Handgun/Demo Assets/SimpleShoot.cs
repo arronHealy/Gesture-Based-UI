a885b8d65f23991a87a60152bb58e0b7cd3e8097
arronHealy <G00346883@gmit.ie>
2020-02-12 00:15:17 +0000

    added test scene, sounds for gun, gesture reload functionality and text to show ammo

--- Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
+++ Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class SimpleShoot : MonoBehaviour
 {
+    public int maxAmmo = 10;
+    private int currentAmmo;
+
+    public TMPro.TextMeshPro text;
 
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
     public Transform barrelLocation;
     public Transform casingExitLocation;
+
+    public AudioSource source;
+    public AudioClip fire;
+    public AudioClip reload;
+    public AudioClip noAmmo;
+
 
 
     public float shotPower = 100f;
@@ -18,18 +28,42 @@ public class SimpleShoot : MonoBehaviour
     {
         if (barrelLocation == null)
             barrelLocation = transform;
+
+        Reload();
     }
 
     void Update()
     {
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
         {
-            GetComponent<Animator>().SetTrigger("Fire");
+            if (currentAmmo > 0)
+            {
+                GetComponent<Animator>().SetTrigger("Fire");
+            }
+            else
+            {
+                source.PlayOneShot(noAmmo);
+            }
         }
+
+        if(Vector3.Angle(transform.up, Vector3.up) > 100 && currentAmmo < maxAmmo)
+        {
+            Reload();
+        }
+
+        text.text = currentAmmo.ToString();
+    }
+
+    void Reload()
+    {
+        currentAmmo = maxAmmo;
+        source.PlayOneShot(reload);
     }
 
     void Shoot()
     {
+        currentAmmo--;
+        source.PlayOneShot(fire);
         //  GameObject bullet;
         //  bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
         // bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
